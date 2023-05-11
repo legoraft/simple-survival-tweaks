cbd46afb86a9323ef51c897872ef63efcbdd97a4
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-05-11 08:42:25 +0200

    added start message and simple boolean for config

---
+++ src/main/java/com/survivaltweaks/config.java
@@ -0,0 +1,7 @@
+package com.survivaltweaks;
+
+public class config {
+
+    public static boolean SURVIVAL_DEBUG_STICK = true;
+
+}
---
+++ src/main/java/com/survivaltweaks/survivalTweaks.java
@@ -0,0 +1,15 @@
+package com.survivaltweaks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class survivalTweaks implements ModInitializer {
+
+    public static final String ID = "survivaltweaks";
+
+    public void onInitialize()  {
+
+        System.out.println("Simple Survival Tweaks loaded!");
+
+    }
+
+}
