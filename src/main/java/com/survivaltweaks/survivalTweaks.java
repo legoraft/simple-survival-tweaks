ddbd883c973fa4536a4630f74590afc2da5566d1
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-05-11 09:45:31 +0200

    fully added configuration file

--- src/main/java/com/survivaltweaks/survivalTweaks.java
+++ src/main/java/com/survivaltweaks/survivalTweaks.java
@@ -4,10 +4,11 @@ import net.fabricmc.api.ModInitializer;
 
 public class survivalTweaks implements ModInitializer {
 
-    public static final String ID = "survivaltweaks";
+    public static final config CONFIG = new config();
 
     public void onInitialize()  {
 
+        CONFIG.load();
         System.out.println("Simple Survival Tweaks loaded!");
 
     }
