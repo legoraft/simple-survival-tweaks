32ffa71563cbafb236ba4f5d5ed3aaafe65e021e
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-05-12 08:36:26 +0200

    made all values default to false

--- src/main/java/com/survivaltweaks/config.java
+++ src/main/java/com/survivaltweaks/config.java
@@ -10,19 +10,22 @@ import java.util.Properties;
 
 public class config {
 
-    public static boolean SURVIVAL_DEBUG_STICK = true;
-    public static boolean NO_EXPENSIVE = true;
+    public static boolean SURVIVAL_DEBUG_STICK = false;
+    public static boolean NO_EXPENSIVE = false;
+    public static boolean CHEAP_RENAME = false;
 
     private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("survivaltweaks.properties");
 
     public void write(Properties properties) {
         properties.setProperty("survival_debug_stick", Boolean.toString(SURVIVAL_DEBUG_STICK));
         properties.setProperty("no_too_expensive", Boolean.toString(NO_EXPENSIVE));
+        properties.setProperty("cheap_rename", Boolean.toString(CHEAP_RENAME));
     }
 
     public void read(Properties properties) {
         SURVIVAL_DEBUG_STICK = Boolean.parseBoolean(properties.getProperty("survival_debug_stick"));
         NO_EXPENSIVE = Boolean.parseBoolean(properties.getProperty("no_too_expensive"));
+        CHEAP_RENAME = Boolean.parseBoolean(properties.getProperty("cheap_rename"));
     }
 
     public void save() {
