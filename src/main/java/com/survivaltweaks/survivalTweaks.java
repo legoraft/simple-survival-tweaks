9acd0e38f5770113d56a65f09952922db6dd9047
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-05-13 13:36:07 +0200

    added debug stick trade, as requested in #2

--- src/main/java/com/survivaltweaks/survivalTweaks.java
+++ src/main/java/com/survivaltweaks/survivalTweaks.java
@@ -1,16 +1,24 @@
 package com.survivaltweaks;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.village.TradeOffer;
 
 public class survivalTweaks implements ModInitializer {
 
     public static final config CONFIG = new config();
 
     public void onInitialize()  {
-
         CONFIG.load();
+        addDebugStickTrade();
         System.out.println("Simple Survival Tweaks loaded!");
+    }
 
+    private static void addDebugStickTrade() {
+        ItemStack debugStick = Items.DEBUG_STICK.getDefaultStack();
+        TradeOfferHelper.registerWanderingTraderOffers(2, factory -> factory.add((((entity, random) -> new TradeOffer(new ItemStack(Items.EMERALD, 5), debugStick, 2, 1, 1.0F)))));
     }
 
 }
