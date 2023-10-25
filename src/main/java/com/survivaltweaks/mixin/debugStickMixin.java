bb3338f644c7c1c0f56603382b570527fe5f06c6
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-10-25 20:53:23 +0200

    debug stick can now cycle through properties

--- src/main/java/com/survivaltweaks/mixin/debugStickMixin.java
+++ src/main/java/com/survivaltweaks/mixin/debugStickMixin.java
@@ -1,21 +1,74 @@
 package com.survivaltweaks.mixin;
 
-import com.survivaltweaks.config;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.DebugStickItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.Registries;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Property;
+import net.minecraft.text.Text;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldAccess;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Collection;
 
 @Mixin(DebugStickItem.class)
-public class debugStickMixin {
+public abstract class debugStickMixin {
 
-    @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isCreativeLevelTwoOp()Z"))
-    public boolean isCreativeLevelTwoOp(PlayerEntity playerEntity) {
-        if (config.SURVIVAL_DEBUG_STICK) {
-            return true;
+    @Shadow
+    private static void sendMessage(PlayerEntity player, Text message) { }
+
+    @Shadow
+    private static <T extends Comparable<T>> BlockState cycle(BlockState state, Property<T> property, boolean inverse) { return null; }
+
+    @Shadow
+    private static <T extends Comparable<T>> String getValueString(BlockState state, Property<T> property) { return null; }
+
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    private void survivalDebug(PlayerEntity player, BlockState state, WorldAccess world, BlockPos pos, boolean update, ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (player.isCreativeLevelTwoOp()) { return; }
+
+        Block block = state.getBlock();
+        StateManager<Block, BlockState> stateManager = block.getStateManager();
+        Collection<Property<?>> collection = stateManager.getProperties();
+
+        NbtCompound nbtCompound = stack.getOrCreateSubNbt("debugProp");
+
+        String blockName = Registries.BLOCK.getId(block).toString();
+        String propertyName = nbtCompound.getString(blockName);
+
+        Property<?> property = stateManager.getProperty(propertyName);
+
+        if (player.isSneaking()) {
+            property = getNextProperty(collection, property);
+            nbtCompound.putString(blockName, property.getName());
+            sendMessage(player, Text.of(String.format("Selected \"%s\" (%s)", property.getName(), getValueString(state, property))));
+        } if (collection.isEmpty() || property == null) {
+            sendMessage(player, Text.of(String.format("%s has no properties", blockName)));
+            return;
+        } else {
+            BlockState newState = cycle(state, property, false);
+            world.setBlockState(pos, newState, 18);
+            sendMessage(player, Text.of(String.format("\"%s\" to %s", property.getName(), getValueString(newState, property))));
         }
-        return playerEntity.isCreativeLevelTwoOp();
+        cir.setReturnValue(true);
+    }
+
+    @Unique
+    private Property<?> getNextProperty(Collection<Property<?>> collection, @Nullable Property<?> property) {
+        property = Util.next(collection, property);
+        return property;
     }
 
 }
