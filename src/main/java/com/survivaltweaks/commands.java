fe0fed3b06f34ad959a83b556b71ab11682d6624
legoraft <50689727+legoraft@users.noreply.github.com>
2023-11-08 09:45:37 +0100

    FIxed translation strings and command strings

--- src/main/java/com/survivaltweaks/commands.java
+++ src/main/java/com/survivaltweaks/commands.java
@@ -14,52 +14,52 @@ public class commands {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment registrationEnvironment) {
 
         dispatcher.register(CommandManager.literal("survivaltweaks")
-                .then(CommandManager.literal("noExpensive").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query" + config.NO_EXPENSIVE, "noExpensive")); return 1;})
+                .then(CommandManager.literal("noExpensive").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query", "noExpensive", config.NO_EXPENSIVE)); return 1;})
                         .then(CommandManager.argument("boolean", bool())
                                 .executes(c -> {
                                     config.NO_EXPENSIVE = getBool(c, "boolean");
                                     applyChanges();
-                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set" + config.NO_EXPENSIVE, "noExpensive"));
+                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set", "noExpensive", config.NO_EXPENSIVE));
                                     return 1;
                                 })
                         )
                 )
-                .then(CommandManager.literal("noEndermanGriefing").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query" + config.NO_ENDERMAN_GRIEF, "NoEndermanGriefing")); return 1;})
+                .then(CommandManager.literal("noEndermanGriefing").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query", "NoEndermanGriefing", config.NO_ENDERMAN_GRIEF)); return 1;})
                         .then(CommandManager.argument("boolean", bool())
                                 .executes(c -> {
                                     config.NO_ENDERMAN_GRIEF = getBool(c, "boolean");
                                     applyChanges();
-                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set" + config.NO_ENDERMAN_GRIEF, "NoEndermanGriefing"));
+                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set", "NoEndermanGriefing", config.NO_ENDERMAN_GRIEF));
                                     return 1;
                                 })
                         )
                 )
-                .then(CommandManager.literal("survivalDebugStick").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query" + config.SURVIVAL_DEBUG_STICK, "survivalDebugStick")); return 1;})
+                .then(CommandManager.literal("survivalDebugStick").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query", "survivalDebugStick", config.SURVIVAL_DEBUG_STICK)); return 1;})
                         .then(CommandManager.argument("boolean", bool())
                                 .executes(c -> {
                                     config.SURVIVAL_DEBUG_STICK = getBool(c, "boolean");
                                     applyChanges();
-                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set" + config.SURVIVAL_DEBUG_STICK, "survivalDebugStick"));
+                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set", "survivalDebugStick", config.SURVIVAL_DEBUG_STICK));
                                     return 1;
                                 })
                         )
                 )
-                .then(CommandManager.literal("cheapRename").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query" + config.CHEAP_RENAME, "cheapRename")); return 1;})
+                .then(CommandManager.literal("cheapRename").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query", "cheapRename", config.CHEAP_RENAME)); return 1;})
                         .then(CommandManager.argument("boolean", bool())
                                 .executes(c -> {
                                     config.CHEAP_RENAME = getBool(c, "boolean");
                                     applyChanges();
-                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set" + config.CHEAP_RENAME, "cheapRename"));
+                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set", "cheapRename", config.CHEAP_RENAME));
                                     return 1;
                                 })
                         )
                 )
-                .then(CommandManager.literal("noXpPenalty").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query" + config.NO_XP_PENALTY, "noXpPenalty")); return 1;})
+                .then(CommandManager.literal("noXpPenalty").executes(c -> { c.getSource().sendMessage(Text.translatable("commands.tweak.query", "noXpPenalty", config.NO_XP_PENALTY)); return 1;})
                         .then(CommandManager.argument("boolean", bool())
                                 .executes(c -> {
                                     config.NO_XP_PENALTY = getBool(c, "boolean");
                                     applyChanges();
-                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set" + config.NO_XP_PENALTY, "noXpPenalty"));
+                                    c.getSource().sendMessage(Text.translatable("commands.tweak.set", "noXpPenalty", config.NO_XP_PENALTY));
                                     return 1;
                                 })
                         )
