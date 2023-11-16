8f8672abd5ee503fa3121aff1dabb38ef4f89882
legoraft <50689727+legoraft@users.noreply.github.com>
2023-11-16 09:30:43 +0100

    got worldSavePath working

--- src/main/java/com/survivaltweaks/survivalTweaks.java
+++ src/main/java/com/survivaltweaks/survivalTweaks.java
@@ -1,6 +1,11 @@
 package com.survivaltweaks;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.WorldSavePath;
+
+import java.nio.file.Path;
 
 public class survivalTweaks implements ModInitializer {
 
@@ -10,6 +15,11 @@ public class survivalTweaks implements ModInitializer {
         CONFIG.load();
         survivalTweaksRegistries.registerTweaks();
         System.out.println("Simple Survival Tweaks loaded!");
+
+        ServerWorldEvents.LOAD.register(((server, world) -> {
+            Path config = server.getSavePath(WorldSavePath.ROOT);
+            System.out.println(config);
+        }));
     }
 
 }
