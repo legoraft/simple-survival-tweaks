52fe6d7e0c801ab22d6c90735e384d9a55ac6c76
legoraft <legoraft@legoraft.com>
2024-08-16 21:16:22 +0200

    Changed debug stick a bit

--- src/main/java/com/survivaltweaks/mixin/debugStickMixin.java
+++ src/main/java/com/survivaltweaks/mixin/debugStickMixin.java
@@ -1,20 +1,26 @@
 package com.survivaltweaks.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.survivaltweaks.config;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.DebugStickItem;
+import net.minecraft.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(DebugStickItem.class)
-public abstract class debugStickMixin {
+public abstract class debugStickMixin extends Item {
 
-    @ModifyExpressionValue(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isCreativeLevelTwoOp()Z"))
-    private boolean survivalDebugStick(boolean original) {
+    public debugStickMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isCreativeLevelTwoOp()Z"))
+    private boolean survivalDebug(PlayerEntity player) {
         if (config.SURVIVAL_DEBUG_STICK) {
             return true;
         } else {
-            return original;
+            return player.isCreativeLevelTwoOp();
         }
     }
 
